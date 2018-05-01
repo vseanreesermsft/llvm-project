e3880e097884a35104d39dbce77426227e988b7e
Ludovic Henry <luhenry@microsoft.com>
2018-05-01 11:18:57 -0400

    Bump debian package version to 6.0 since we are based on LLVM 6.0

--- llvm/scripts/ci/package.sh
+++ llvm/scripts/ci/package.sh
@@ -9,14 +9,14 @@ TOP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../../" && pwd )"
 
 echo "MAKING DEB"
 
-mkdir -p $TOP_DIR/build/mono-llvm-3.9
-pushd $TOP_DIR/build/mono-llvm-3.9
+mkdir -p $TOP_DIR/build/mono-llvm-6.0
+pushd $TOP_DIR/build/mono-llvm-6.0
 
 cp -r $TOP_DIR/scripts/ci/debian debian
 
 dpkg-buildpackage -d -us -uc
 
-popd # $TOP_DIR/build/mono-llvm-3.9
+popd # $TOP_DIR/build/mono-llvm-6.0
 
 echo "DONE"
 
