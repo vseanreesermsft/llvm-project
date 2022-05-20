37d37942217b7103abda5091db2af3be9dedce3f
Alexander Köplinger <alex.koeplinger@outlook.com>
2022-05-20 17:20:28 +0200

    Disable LLVM tests in llvm_create_cross_target
    This works around an issue where we'd require Python 3.6 even though building llvm itself doesn't require it.

--- llvm/cmake/modules/CrossCompile.cmake
+++ llvm/cmake/modules/CrossCompile.cmake
@@ -85,6 +85,7 @@ function(llvm_create_cross_target project_name target_name toolchain buildtype)
         ${external_project_source_dirs}
         -DLLVM_TEMPORARILY_ALLOW_OLD_TOOLCHAIN="${LLVM_TEMPORARILY_ALLOW_OLD_TOOLCHAIN}"
         -DLLVM_TOOLS_TO_BUILD="all"
+        -DLLVM_INCLUDE_TESTS=OFF
         ${build_type_flags} ${linker_flag} ${external_clang_dir}
         ${ARGN}
     WORKING_DIRECTORY ${${project_name}_${target_name}_BUILD}
