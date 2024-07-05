7c8313aafeac54550d2c1c0b2ec4c6488c91fa01
Vlad Brezae <brezaevlad@gmail.com>
2024-07-05 12:01:09 +0300

    Update value of mono cconv enum (#589)

--- llvm/include/llvm-c/Core.h
+++ llvm/include/llvm-c/Core.h
@@ -221,7 +221,7 @@ typedef enum {
   LLVMPreserveAllCallConv   = 15,
   LLVMSwiftCallConv         = 16,
   LLVMCXXFASTTLSCallConv    = 17,
-  LLVMMono1CallConv         = 21,
+  LLVMMono1CallConv         = 22,
   LLVMX86StdcallCallConv    = 64,
   LLVMX86FastcallCallConv   = 65,
   LLVMARMAPCSCallConv       = 66,
